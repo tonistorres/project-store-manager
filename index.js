15b70f3045825c9e92e3670f6ae434c152490bc0
tonistorres <sisvenda2011@gmail.com>
2022-03-30 17:50:33 -0300

    feat: implementando index.js, criando camada routes

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
 require('dotenv').config();
+const router = require('./routes/route');
+
+const app = express(); 
+app.use(bodyParser.json()); 
+app.use(express.json()); 
+app.use('/user', router);
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
