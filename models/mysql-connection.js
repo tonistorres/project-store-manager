10aa05e43ec2ad7767aed69968e487c1912b4550
tonistorres <sisvenda2011@gmail.com>
2022-03-30 17:33:00 -0300

    feat: implementando arq mysql-connections

---
+++ models/mysql-connection.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const mysql = require('mysql2/promise');
+
+ const connection = mysql.createPool({
+   host: process.env.MYSQL_HOST,
+   user: process.env.MYSQL_USER,
+   database: process.env.MYSQL_DB,
+   password: process.env.MYSQL_PASSWORD,
+   port: process.env.MYSQL_PORT,
+ });
+ 
+ module.exports = { connection };
