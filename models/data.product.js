edbe4a6c09d9b33a375a265b7584a47ebbc4f08c
tonistorres <sisvenda2011@gmail.com>
2022-04-04 10:58:13 -0300

    refactor: refatorando querys model add banco

--- models/data.product.js
+++ models/data.product.js
@@ -1,24 +1,24 @@
 const { connection } = require('./mysql-connection');
 
 const getAllModelProducts = async () => {
-    const [products] = await connection.execute('SELECT * FROM products ORDER BY id ASC');
+    const [products] = await connection.execute('SELECT * FROM StoreManager.products ORDER BY id ASC');
     return products;
   };
 
 const getByIdModelProduct = async (id) => {
-    const [product] = await connection.execute('SELECT * FROM products WHERE id = ?', [id]);
+    const [product] = await connection.execute('SELECT * FROM StoreManager.products WHERE id = ?', [id]);
     return product[0];
   };
 
 // validar 
   const getByNameModelProduct = async (name) => {
-    const [product] = await connection.execute('SELECT * FROM products WHERE name = ?', [name]);
+    const [product] = await connection.execute('SELECT * FROM StoreManager.products WHERE name = ?', [name]);
     return product[0];
   };
 
 const createModelProduct = async ({ name, quantity }) => {
   const [{ insertId }] = await connection.execute(`INSERT INTO
-   products (name, quantity) VALUES (?, ?)`, [name, quantity]);
+  StoreManager.products (name, quantity) VALUES (?, ?)`, [name, quantity]);
     return {
         id: insertId,
         name,
@@ -27,7 +27,7 @@ const createModelProduct = async ({ name, quantity }) => {
 };
 
 const updateModelProduct = async ({ id, name, quantity }) => {
-  await connection.execute('UPDATE products SET name = ?, quantity = ? WHERE id = ?',
+  await connection.execute('UPDATE StoreManager.products SET name = ?, quantity = ? WHERE id = ?',
       [name, quantity, id]);
   return {
       id,
@@ -37,7 +37,7 @@ const updateModelProduct = async ({ id, name, quantity }) => {
 };
 
 const deletModelByIdProduct = async (id) => {
-  await connection.execute('DELETE FROM products WHERE id = ?', [id]);
+  await connection.execute('DELETE FROM StoreManager.products WHERE id = ?', [id]);
 };
 
 async function main() {
